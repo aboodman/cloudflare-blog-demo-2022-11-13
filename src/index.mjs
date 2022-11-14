816a8a26261a06d2c406b99bf0c2d837080914d4
Aaron Boodman <aaron@aaronboodman.com>
2022-11-13 15:20:22 -1000

    durablish

--- src/index.mjs
+++ src/index.mjs
@@ -2,13 +2,17 @@
 
 export default {
   async fetch(request, env) {
-    return await handleRequest(request, env);
-  }
-}
+    const url = new URL(request.url);
+    if (url.pathname === "/durablish") {
+      return await handleRequest(request, env.DURABLISH);
+    }
+    return new Response("not found", { status: 404 });
+  },
+};
 
-async function handleRequest(request, env) {
-  let id = env.COUNTER.idFromName("A");
-  let obj = env.COUNTER.get(id);
+async function handleRequest(request, namespace) {
+  let id = namespace.idFromName("A");
+  let obj = namespace.get(id);
   let resp = await obj.fetch(request.url);
   let count = await resp.text();
 
@@ -17,41 +21,13 @@ async function handleRequest(request, env) {
 
 // Durable Object
 
-export class Counter {
-  constructor(state, env) {
-    this.state = state;
+export class DurableishCounter {
+  constructor() {
+    this.count = 0;
   }
 
   // Handle HTTP requests from clients.
-  async fetch(request) {
-    // Apply requested action.
-    let url = new URL(request.url);
-
-    // Durable Object storage is automatically cached in-memory, so reading the
-    // same key every request is fast. (That said, you could also store the
-    // value in a class member if you prefer.)
-    let value = await this.state.storage.get("value") || 0;
-
-    switch (url.pathname) {
-    case "/increment":
-      ++value;
-      break;
-    case "/decrement":
-      --value;
-      break;
-    case "/":
-      // Just serve the current value.
-      break;
-    default:
-      return new Response("Not found", {status: 404});
-    }
-
-    // We don't have to worry about a concurrent request having modified the
-    // value in storage because "input gates" will automatically protect against
-    // unwanted concurrency. So, read-modify-write is safe. For more details,
-    // see: https://blog.cloudflare.com/durable-objects-easy-fast-correct-choose-three/
-    await this.state.storage.put("value", value);
-
-    return new Response(value);
+  async fetch() {
+    return new Response(++this.count);
   }
 }
